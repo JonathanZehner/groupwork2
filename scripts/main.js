93a443e7d47de8aaf1458c57b737c77d75a25525
Jonathan A. Zehner <78240659+JonathanZehner@users.noreply.github.com>
2021-07-30 14:54:28 -0400

    added hamburger menu code and smoothscroll

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+// ************* Hamburger Menu ***************/
+// https://codepen.io/g13nn/pen/eHGEF
+
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+  $( ".hamburger" ).hide();
+  $( ".cross" ).show();
+$( ".menu" ).slideToggle( "slow", function() {
+});
+});
+
+$( ".cross" ).click(function() {
+  $( ".cross" ).hide();
+  $( ".hamburger" ).show();
+$( ".menu" ).slideToggle( "slow", function() {
+});
+});
+
+});
+// ************* End Hamburger Menu **********/
